e59663c628785f2ea7bab7ef3bdc20474f3c91c8
kenchangh <guanhao3797@gmail.com>
2021-05-28 00:16:44 +0100

    Working setup

---
+++ contracts/UniswapOracle.sol
@@ -0,0 +1,7 @@
+//SPDX-License-Identifier: GPL-3.0
+pragma solidity 0.7.3;
+
+
+contract UniswapOracle {
+
+}
--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,9 +1,10 @@
-require("@nomiclabs/hardhat-waffle");
+import { task } from "hardhat/config";
+import "@nomiclabs/hardhat-waffle";
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
 task("accounts", "Prints the list of accounts", async () => {
-  const accounts = await ethers.getSigners();
+  const accounts = await hre.ethers.getSigners();
 
   for (const account of accounts) {
     console.log(account.address);
@@ -16,7 +17,7 @@ task("accounts", "Prints the list of accounts", async () => {
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
-module.exports = {
+
+ export default {
   solidity: "0.7.3",
 };
-
