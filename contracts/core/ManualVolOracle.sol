9bf71f8072a37c9d5bd7f81858a4c9e01fb54fe0
Steven <steven@ribbon.finance>
2022-03-02 17:33:29 +0700

    Modified setAnnualizedVol for multiple pools

--- contracts/core/ManualVolOracle.sol
+++ contracts/core/ManualVolOracle.sol
@@ -9,6 +9,8 @@ contract ManualVolOracle is AccessControl {
 
     mapping(address => uint256) private annualizedVols;
 
+    mapping(address => bool) private poolHistory;
+
     /**
      * @notice Creates an volatility oracle for a pool
      * @param _admin is the admin
@@ -48,16 +50,37 @@ contract ManualVolOracle is AccessControl {
     }
 
     /**
-     * @notice Sets the annualized standard deviation of the base currency of the `pool`
-     * @param _pool is the uniswap pool we want to set annualized volatility for
-     * @param _annualizedVol is the annualized volatility with 10**8 decimals i.e. 1*10**8 = 100%
+     * @notice Sets the annualized standard deviation of the base currency of one or more `pool(s)`
+     * @param _pools is an array of uniswap pools we want to set annualized volatility for
+     * @param _newAnnualizedVols is an array of the annualized volatility with 10**8 decimals i.e. 1*10**8 = 100%
      */
-    function setAnnualizedVol(address _pool, uint256 _annualizedVol)
-        external
-        onlyAdmin
-    {
-        require(_annualizedVol > 50 * 10**6, "Cannot be less than 50%");
-        require(_annualizedVol < 400 * 10**6, "Cannot be more than 400%");
-        annualizedVols[_pool] = _annualizedVol;
+    function setAnnualizedVol(
+        address[] calldata _pools,
+        uint256[] calldata _newAnnualizedVols
+    ) external onlyAdmin {
+        require(
+            _pools.length == _newAnnualizedVols.length,
+            "Input lengths mismatched"
+        );
+
+        for (uint256 i = 0; i < _pools.length; i++) {
+            address pool = _pools[i];
+            uint256 newAnnualizedVol = _newAnnualizedVols[i];
+
+            require(
+                !poolHistory[pool],
+                "Cannot set vol for the same pool twice"
+            );
+            require(newAnnualizedVol > 50 * 10**6, "Cannot be less than 50%");
+            require(newAnnualizedVol < 400 * 10**6, "Cannot be more than 400%");
+
+            annualizedVols[pool] = newAnnualizedVol;
+            poolHistory[pool] = true;
+        }
+
+        // Set mapping back to false for gas refunds
+        for (uint256 i = 0; i < _pools.length; i++) {
+            poolHistory[_pools[i]] = false;
+        }
     }
 }
