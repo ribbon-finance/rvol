6df48fdebfed457aa2a2a650785b07b89739db85
Steven <steven@ribbon.finance>
2022-03-10 15:55:56 +0800

    Fixed solidity version

--- contracts/interfaces/IPriceOracle.sol
+++ contracts/interfaces/IPriceOracle.sol
@@ -1,5 +1,5 @@
 //SPDX-License-Identifier: GPL-3.0
-pragma solidity ^0.7.3;
+pragma solidity >0.6.0 <0.8.7;
 
 interface IPriceOracle {
     function decimals() external view returns (uint256 _decimals);
